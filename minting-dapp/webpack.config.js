f5005ca214c142f7ede2ffd29d5b3345af45d0c2
Benjamin Howes <91109173+capnganj@users.noreply.github.com>
2022-03-10 00:14:34 -0500

    github pages try 4

--- minting-dapp/webpack.config.js
+++ minting-dapp/webpack.config.js
@@ -10,12 +10,11 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    //.setOutputPath('public/build/')
-    .setOutputPath('../docs/PoisonToadsDapp/')
+    .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/PoisonToadsDapp')
+    .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    .setManifestKeyPrefix('PoisonToadsDapp/')
+    .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
